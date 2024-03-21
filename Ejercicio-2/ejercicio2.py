0de687e62f58e4e9bd93c62717508c9022e9030c
Luis Adames <adames1601@gmail.com>
2024-03-21 00:32:34 -0400

    Ejercicio 2 completado

--- Ejercicio-2/ejercicio2.py
+++ Ejercicio-2/ejercicio2.py
@@ -1,35 +1,49 @@
 # Problema Formula 1
 
 def score():
-    G, Q = map(int, input().split())
-    if G == 0:
-        return 0
-    
-    g = [[0] * 105 for i in range(105)]
-    
-    for i in range(G):
-        row = list(map(int, input().strip().split()))
-        for j, x in enumerate(row):
-            g[i][x - 1] = j + 1
+    G, P = 1, 1
+    ganadoresTodos = []
+    while True:
+        print("Carreras y pilotos")
+        G, P = map(int, input().split())
+        
+        if(G == 0 and P == 0):
+            break
+        
+        results = []
+        print("\nResultados de las carreras")
+        for i in range(G):
+            resultado = list(map(int, input().strip().split()))
+            results.append(resultado)
+        
+        print("\nNúmero de sistemas de puntajes")
+        S = int(input().strip())
+        
+        print("\nSistema de puntaje")
+        for i in range(S):
             
-    S = int(input().strip())
-    for i in range(S):
-        score = [0] * 105
-        K = int(input().strip())
-        for j in range(K):
-            row = list(map(int, input().strip().split()))
-            for p in range(G):
-                score[g[p][row[p] - 1]] += row[p]
-                
-        mx = max(score)
-        first = True
-        for i in range(1, Q + 1):
-            if score[i] == mx:
-                if first:
-                    print(i, end="")
-                    first = False
-                else:
-                    print(" ", i, end="")
-        print()
-
+            
+            sistema = list(map(int, input().split()))[1:]
+            puntajes = [0 for _ in range(P)]
+            
+            for resultado in results:
+                for piloto, posicion in enumerate(resultado, start=1):
+                    if posicion <= len(sistema):
+                        puntajes[piloto - 1] += sistema[posicion - 1]
+            
+            max_puntaje = max(puntajes)
+            
+            ganadores = []
+            
+            for score, puntaje in enumerate(puntajes):
+                if puntaje == max_puntaje:
+                    ganadores.append(score + 1)
+            
+            ganadoresTodos.append(ganadores)
+    
+    print("\nLos ganadores son: ")
+    for ganadores in ganadoresTodos:
+        print(" ".join(map(str, ganadores)))
+        
 score()
+
